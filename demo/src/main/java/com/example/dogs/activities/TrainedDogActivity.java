a1a034e62f0221d1134f843f55a7fd048eeaef75
Katrusya <ruda.venera@gmail.com>
2024-07-19 19:01:05 +0300

    TrainedDogActivity created

---
+++ demo/src/main/java/com/example/dogs/activities/TrainedDogActivity.java
@@ -0,0 +1,8 @@
+package com.example.dogs.activities;
+
+public class TrainedDogActivity extends DogActivity {
+
+    public void performActivity() {
+        System.out.println("Собака охороняє будинок.");
+    }
+}
